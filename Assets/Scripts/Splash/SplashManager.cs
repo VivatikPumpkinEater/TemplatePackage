3a24c2f7662b5616e4be78336f1908c0e334ffba
Ivanus <vivatik.pumpkin.eater@gmail.com>
2024-07-03 11:45:54 +0300

    Init

---
+++ Assets/Scripts/Splash/SplashManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace Splash
+{
+    public class SplashManager : MonoBehaviour
+    {
+        private void Start()
+        {
+            Addressables.LoadSceneAsync("Assets/Scenes/Game.unity");
+        }
+    }
+}
